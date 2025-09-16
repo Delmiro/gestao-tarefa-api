ca5454f46172b14ff38104ab7615e19f0accd37a
diego.delmiro <diego.delmiro@globalhitss.com.br>
2025-09-15 21:06:35 -0300

    Inicialização do projeto - primeiro commit .

---
+++ src/test/java/com/gestaotarefa/br/GestaotarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaotarefa.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaotarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
