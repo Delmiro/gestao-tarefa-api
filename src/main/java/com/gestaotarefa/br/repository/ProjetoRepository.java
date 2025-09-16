ffe11e48c1fc305ac2376e1051668abfbd2d1cbe
diego.delmiro <diego.delmiro@globalhitss.com.br>
2025-09-15 21:27:18 -0300

    Criado os repository das entidades do projeto .

---
+++ src/main/java/com/gestaotarefa/br/repository/ProjetoRepository.java
@@ -0,0 +1,6 @@
+package com.gestaotarefa.br.repository;
+
+import com.gestaotarefa.br.domain.entity.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> { }
