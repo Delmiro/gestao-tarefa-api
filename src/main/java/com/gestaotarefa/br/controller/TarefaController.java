08e6ddcf967d7458de62854ef5bf947b364d7890
diego.delmiro <diego.delmiro@globalhitss.com.br>
2025-09-16 18:01:12 -0300

    Criado camada controller da api .

---
+++ src/main/java/com/gestaotarefa/br/controller/TarefaController.java
@@ -0,0 +1,37 @@
+package com.gestaotarefa.br.controller;
+
+import com.gestaotarefa.br.domain.dto.TarefaDTO;
+import com.gestaotarefa.br.service.TarefaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tarefas")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+public class TarefaController {
+
+    private final TarefaService tarefaService;
+
+    @PostMapping
+    public ResponseEntity<TarefaDTO> criar(@RequestBody TarefaDTO dto) {
+        return ResponseEntity.ok(tarefaService.criar(dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        tarefaService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TarefaDTO>> listar(
+            @RequestParam(required = false) Long idProjeto,
+            Pageable pageable
+    ) {
+        return ResponseEntity.ok(tarefaService.listar(idProjeto, pageable));
+    }
+}
